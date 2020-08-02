32cba1b4d3f7faf1ff4582fdce89fe735161a877
Magdalena Borettini <magdalena.borettini@gmail.com>
2020-08-02 11:07:38 +0200

    Initial commit

---
+++ src/main/java/com/borettini/HomeServlet.java
@@ -0,0 +1,16 @@
+package com.borettini;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class HomeServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter writer = response.getWriter();
+        writer.println("Hello");
+    }
+}
